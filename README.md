d991a8c8e819daa132cce26ea5bc6f321fcc138b
David Custódio <davidlacustodio@gmail.com>
2020-03-20 09:26:13 +0100

    feat: allow partial file generation with from args flag. args local flag to operate over local files

--- README.md
+++ README.md
@@ -12,6 +12,15 @@ This will create 6 files:
  - 3 files (deaths.csv, confirmed.csv, recovered.csv) from the Novel Covid-19 Data repository
  - 3 files (deaths-rows.csv, confirmed-rows.csv, recovered-rows.csv) with the transformed data. 
 
+`node index.js --from 19-03-2020`
+
+This will create 3 files with the records from that day (included) onwards: 
+ - 3 files (deaths-19-03-2020rows.csv, confirmed-19-03-2020rows.csv, recovered-19-03-2020rows.csv) with the transformed data. 
+
+`node index.js --local`
+
+Once you got the main files (updated every day) you can just use the local versions using the `--local` flag.
+
 ## Example output
 
 `deaths-rows.csv`
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const lineReader = require('line-reader')
 const fs = require('fs')
-var moment = require('moment')
+const moment = require('moment')
 const https = require('https')
+const argv = require('yargs').argv
 
 const FILES =
 {
@@ -10,45 +11,75 @@ const FILES =
   recovered: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
 }
 
-Object.keys(FILES).forEach(name => {
-  https.get(FILES[name], response => {
-    const file = fs.createWriteStream(`${name}.csv`)
-    var stream = response.pipe(file)
+// --from 19-03-2020
+const from = argv.from
+// --local
+const local = typeof argv.local !== 'undefined' && argv.local
 
-    stream.on('finish', function () {
-      let lineCount = 0
-      let dates
+if (from && !(/^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[012])-20\d\d$/.test(from))) {
+  console.error('invalid date format. please use DD-MM-YYYY')
 
-      var stream = fs.createWriteStream(`${name}-rows.csv`)
-      // headers: Province/State,Country/Region,Lat,Long, Date 1, Date 2, ... , Date n
-      lineReader.eachLine(`${name}.csv`, function (line, last) {
-        // replace line separators because commas are also used in state and country names.
-        line = line.replace(/,(?! )/gm, ';')
-        const cells = line.split(';')
-        const province = addQuotes(cells.shift())
-        const country = addQuotes(cells.shift())
-        const lat = cells.shift()
-        const long = cells.shift()
+  process.exit(1)
+}
 
-        if (lineCount === 0) {
-          dates = cells
-          stream.write(`date;${province};${country};${lat};${long};${name}\n`)
+const writeRowFiles = (name) => () => {
+  let lineCount = 0
+  let dates
+  const OUT_FILENAME = `${name}-${from || ''}rows.csv`
+  const stream = fs.createWriteStream(OUT_FILENAME)
+  // headers: Province/State,Country/Region,Lat,Long, Date 1, Date 2, ... , Date n
+  lineReader.eachLine(`${name}.csv`, function (line, last) {
+    // replace line separators because commas are also used in state and country names.
+    line = line.replace(/,(?! )/gm, ';')
+    const cells = line.split(';')
+    const province = addQuotes(cells.shift())
+    const country = addQuotes(cells.shift())
+    const lat = cells.shift()
+    const long = cells.shift()
+    // table header
+    if (lineCount === 0) {
+      dates = cells
+      stream.write(`date;${province};${country};${lat};${long};${name}\n`)
+    } else {
+      // table body
+      cells.forEach((value, index) => {
+        const cellDate = moment(dates[index], 'M/D/YY').format('DD-MM-YYYY')
+        // print dates from (including it)
+        if (from) {
+          if (moment(cellDate, 'DD-MM-YYYY').isSameOrAfter(moment(from, 'DD-MM-YYYY'))) {
+            stream.write(`${cellDate};${province};${country};${lat};${long};${value}\n`)
+          }
         } else {
-          cells.forEach((value, index) => {
-            if (index < cells.length) {
-              stream.write(`${moment(dates[index], 'M/D/YY').format('DD-MM-YYYY')};${province};${country};${lat};${long};${value}\n`)
-            }
-          })
-        }
-
-        lineCount++
-
-        if (last) {
-          stream.end()
+          // print historic
+          if (index < cells.length) {
+            stream.write(`${cellDate};${province};${country};${lat};${long};${value}\n`)
+          }
         }
       })
-    })
+    }
+
+    lineCount++
+
+    if (last) {
+      console.info(`finished ${OUT_FILENAME}`)
+      stream.end()
+    }
   })
+}
+
+Object.keys(FILES).forEach(name => {
+  // we don't need to download the files each time
+  if (local) {
+    console.info(`[local] ${name}`)
+    writeRowFiles(name)()
+  } else {
+    https.get(FILES[name], response => {
+      console.info(`[http] ${name}`)
+      const file = fs.createWriteStream(`${name}.csv`)
+      var stream = response.pipe(file)
+      stream.on('finish', writeRowFiles(name))
+    })
+  }
 })
 
 function addQuotes (cell) {
